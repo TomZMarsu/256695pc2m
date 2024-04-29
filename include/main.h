a78da3e6c2e6e13304da9f4897f8ee4d6d57ecb3
Tomáš Svoboda <tom24430@gmail.com>
2024-04-29 15:49:12 +0200

    Vyčistění knihoven v main.h

--- include/main.h
+++ include/main.h
@@ -1,14 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include "pozorovani.h"
-#include <malloc.h>
-#include "ptak.h"
-#include "pozorovani.h"
-#include "datum.h"
-#include "vypisovac.h"
-#include "nacitacdat.h"
-#include "stringutil.h"
-#include "ovladac_pozorovani.h"
 #include "ovladac_seznamu_pozorovani.h"
 
 int main();
--- src/main.c
+++ src/main.c
@@ -5,16 +5,12 @@
 Pozorovani* main_prvni_pozorovani = NULL;
 
 int main() {
-    
     vykreslovat_seznam_pozorovani(&main_prvni_pozorovani);
 
     return 0;
 }
 
 void ukoncit_program() {
-    if(main_prvni_pozorovani == NULL) {
-        printf("ZASE NULA\n\n\n");
-    }
     smazat_seznam_pozorovani(main_prvni_pozorovani);
 
     exit(EXIT_SUCCESS);
